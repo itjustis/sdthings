5ae1ba56e9bae6d2fb23047773aac1e047ec1304
itjustis <98639820+itjustis@users.noreply.github.com>
2023-01-30 03:37:23 +0800

    Update setup.py

--- scripts/setup.py
+++ scripts/setup.py
@@ -62,11 +62,11 @@ def setup_environment(print_subprocess=True):
     print("..setting up environment")
     #['git', 'clone', '-b', 'dev', 'https://github.com/deforum-art/deforum-stable-diffusion'],
     #
-    
+    #['pip', 'install', 'open_clip_torch'],
+    #['pip', 'install', 'clip-interrogator'],
+    #['pip', 'install', 'git+https://github.com/pharmapsychotic/BLIP.git'],
     all_process = [
-        ['pip', 'install', 'open_clip_torch'],
-        ['pip', 'install', 'clip-interrogator'],
-        ['pip', 'install', 'git+https://github.com/pharmapsychotic/BLIP.git'],
+        
         ['pip', 'install', 'torch==1.12.1+cu113', 'torchvision==0.13.1+cu113', '--extra-index-url', 'https://download.pytorch.org/whl/cu113'],
         ['pip', 'install', 'omegaconf==2.2.3', 'einops==0.4.1', 'pytorch-lightning==1.7.4', 'torchmetrics==0.9.3', 'torchtext==0.13.1', 'transformers==4.21.2', 'safetensors', 'kornia==0.6.7'],
         ['git', 'clone', 'https://github.com/deforum-art/deforum-stable-diffusion'],
